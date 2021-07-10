4f37e6ee0de51422ac8b9b04c17689331a3d8231
Snopphin <81505000+Snopphin@users.noreply.github.com>
2021-07-10 22:45:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -923,61 +923,7 @@ long __fastcall RenderMain()
                 }
             }
         }
-        static int sliderInt2 = 0.f;
-        if (ImGui::SliderInt)
-        if (ImGui::Button("Inject Dll")) { //not my func
-            std::string stringpath = chooseDLL();
-            const char* DllPath = stringpath.c_str();
 
-            // Open a handle to target process
-            HWND hWnd = FindWindow(0, "Geometry Dash");
-
-            if (hWnd == 0)
-            {
-                MessageBox(0, "Error cannot find window.", "Error", MB_OK | MB_ICONERROR);
-            }
-            else
-            {
-                DWORD proccess_ID;
-                GetWindowThreadProcessId(hWnd, &proccess_ID);
-                HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, proccess_ID);
-
-                // Allocate memory for the dllpath in the target process
-                // length of the path string + null terminator
-                LPVOID pDllPath = VirtualAllocEx(hProcess, 0, strlen(DllPath) + 1,
-                    MEM_COMMIT, PAGE_READWRITE);
-
-                // Write the path to the address of the memory we just allocated
-                // in the target process
-                WriteProcessMemory(hProcess, pDllPath, (LPVOID)DllPath,
-                    strlen(DllPath) + 1, 0);
-
-                // Create a Remote Thread in the target process which
-                // calls LoadLibraryA as our dllpath as an argument -> program loads our dll
-                HANDLE hLoadThread = CreateRemoteThread(hProcess, 0, 0,
-                    (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"),
-                        "LoadLibraryA"), pDllPath, 0, 0);
-
-                // Wait for the execution of our loader thread to finish
-                WaitForSingleObject(hLoadThread, INFINITE);
-
-                std::cout << "Dll path allocated at: " << std::hex << pDllPath << std::endl;
-                std::cin.get();
-
-                // Free the memory allocated for our dll path
-                VirtualFreeEx(hProcess, pDllPath, strlen(DllPath) + 1, MEM_RELEASE);
-            }
-
-        }
-
-    
-                ImGui::Text(" %.3f ms (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate); //some test
-                ImGui::End();
-            
-        
-    }
-    return 0;
-}
 
 
 
